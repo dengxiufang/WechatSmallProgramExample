f52369c240fda27a1bdb48d30625227045513aae
dengxiufang <627223510@qq.com>
2018-04-24 14:28:09 +0800

    微信小程序官方文档示例

---
+++ pages/databind/databind.js
@@ -0,0 +1,88 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    message:"Hello MINA",
+    id: 0,
+    condition: true,
+    a:1,
+    b:2,
+    c:3,
+    length:10,
+    name: "alfred",
+    object:{
+      key:'hello'
+    },
+    array: ['alfred'],
+    zero: 0,
+    for: 1,
+    bar: 2,
+    obj1:{
+      for:1,
+      bar:2
+    },
+    item: {
+      index: 0,
+      msg: 'this is a template',
+      time: '2016-09-15'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
